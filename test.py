26775423fa1567880dd8f556ea0ab9e68a9b1323
andreazampieri <andreazampieri95@gmail.com>
2017-12-04 14:35:25 +0100

    test multithread

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ from sklearn.svm import SVC
 from sklearn import metrics
 from sklearn.model_selection import KFold, cross_val_score, learning_curve
 from sklearn.model_selection import GridSearchCV
-import threaded_svm as tSVM
+import threaded_svm as tSVC
 
 # digits = load_digits()
 
@@ -39,7 +39,7 @@ import threaded_svm as tSVM
 # print(cm)
 # print(clf.best_params_)
 
-base = '/sklearn-lab-material/ocr/'
+base = 'sklearn-lab-material/ocr/'
 test_data_path = base +'test-data.csv'
 test_targets_path = base +'test-targets.csv'
 train_data_path = base +'train-data.csv'
@@ -47,28 +47,32 @@ train_target_path = base + 'train-targets.csv'
 
 def slice(data,cardinal,tot):
 	dim = len(data)
-	a = int(data*cardinal*1.0/tot)
-	b = int(data*(cardinal+1)*1.0/tot)
-	return data[0:a].append(data[b:dim])
+	a = int(dim*cardinal*1.0/tot)
+	b = int(dim*(cardinal+1)*1.0/tot)
+	return np.append(data[0:a],data[b:dim])
 
 with open(train_data_path) as file:
-	train_data = np.array([])
+	train_data = []
 	for line in file:
 		train_data.append([int(_) for _ in line.strip().split(',')])
 
 with open(train_target_path) as file:
-	train_target = np.array([])
+	train_target = []
 	for line in file:
 		train_target.append(line.strip())
 
 with open(test_data_path) as file:
-	test_data = np.array([])
+	test_data = []
 	for line in file:
 		test_data.append([int(_) for _ in line.strip().split(',')])
 
+train_data = np.array(train_data)
+train_target = np.array(train_target)
+test_data = np.array(test_data)
+
 c = [1,10,100,1000]
-gamma = [0.1,0.05,0.02,0.01]
-sigma = [1e-1, 1e-2, 1e-3, 1e-4]
+#gamma = [0.1,0.05,0.02,0.01]
+gamma = [1e-1, 1e-2, 1e-3, 1e-4]
 svm = []
 
 idxs = np.array(range(len(train_data)))
@@ -78,12 +82,14 @@ i=0
 n_folds = 5
 for c_i in c:
 	for gamma_i in gamma:
-		for sigma_i in sigma:
-			batch_idx = slice(idxs,i,n_folds)
-			svm.append(tSVM.tSVM(i,'',SVC(C=c_i,kernel='rbf',gamma=gamma_i,sigma=sigma_i),train_data[batch_idx],train_target[batch_idx]))
-			i = (i+1)%n_folds
+		batch_idx = slice(idxs,i,n_folds)
+		svm.append(tSVC.tSVC(i,'',SVC(C=c_i,kernel='rbf',gamma=gamma_i),train_data[batch_idx],train_target[batch_idx]))
+		i = (i+1)%n_folds
 
-print('Classifiers: {}'.format(len(svm))
+print('Classifiers: {}'.format(len(svm)))
 #wait for all training
+for clf in svm:
+	clf.start()
+
 for clf in svm:
 	clf.join()
