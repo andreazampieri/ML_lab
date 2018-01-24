e4e7bbc20f08e65a0e5679218d5cefcc41deef79
andreazampieri <andreazampieri95@gmail.com>
2018-01-24 18:25:24 +0100

    fix params cnn

--- DeepNetwork/cnn.py
+++ DeepNetwork/cnn.py
@@ -34,6 +34,7 @@ def main():
 	train_data_path = base_path + 'train-data.csv'
 	train_labels_path = base_path + 'train-targets.csv'
 	test_data_path = base_path + 'test-data.csv'
+	test_labels_path = base_path + 'test-targets.csv'
 
 	print('Reading data')
 	with open(train_data_path,'r') as file:
@@ -66,36 +67,50 @@ def main():
 	# 	4x2 patches
 	# 	24 filters
 	
-	conv1_nfeats = 24
-	conv1_w = w_var([4,2,1,conv1_nfeats])
+	keep_prob = tf.placeholder(tf.float32)
+	conv1_nfeats = 128
+	patch_height = 8
+	patch_width = 8
+	conv1_w = w_var([patch_height,patch_width,1,conv1_nfeats])
 	conv1_b = b_var([conv1_nfeats])
 
-	conv1 = tf.nn.relu(conv2d(x_rshp,conv1_w)+conv1_b)
+	#conv1 = tf.nn.relu(conv2d(x_rshp,conv1_w)+conv1_b)
+	conv1 = tf.nn.dropout(tf.nn.relu(conv2d(x_rshp,conv1_w)+conv1_b),keep_prob)
 	pool1 = max_pool_2x2(conv1)
 
 	# after the first pooling, the input has shape [batch_size, 8,4, conv1_nfeats]
 	# [,8,4,24]
-	conv2_nfeats = 48
-	conv2_w = w_var([4,2,conv1_nfeats,conv2_nfeats])
+	conv2_nfeats = 256
+	conv2_w = w_var([4,4,conv1_nfeats,conv2_nfeats])
 	conv2_b = b_var([conv2_nfeats])
 
-	conv2 = tf.nn.relu(conv2d(pool1,conv2_w)+conv2_b)
+	#conv2 = tf.nn.relu(conv2d(pool1,conv2_w)+conv2_b)
+	conv2 = tf.nn.dropout(tf.nn.relu(conv2d(pool1,conv2_w)+conv2_b),keep_prob)
 	pool2 = max_pool_2x2(conv2)
 	# pool2 has shape [,4,2,48]; 4*2*48 = 384
 	# 2 fully connected layers of size (resp) 100 and 26
-	keep_prob = tf.placeholder(tf.float32)
 
-	fcl1_w = w_var([4*2*48,100])
-	fcl1_b = b_var([100])
+	fcl1_dim = 128
+	fcl1_w = w_var([4*2*conv2_nfeats,fcl1_dim])
+	fcl1_b = b_var([fcl1_dim])
 
-	pool2_flat = tf.reshape(pool2,[-1,4*2*48])
+	pool2_flat = tf.reshape(pool2,[-1,4*2*conv2_nfeats])
 	fcl1 = tf.nn.relu(tf.matmul(pool2_flat,fcl1_w)+fcl1_b)
 	dropout_fcl1 = tf.nn.dropout(fcl1,keep_prob)
 
-	fcl2_w = w_var([100,26])
-	fcl2_b = b_var([26])
+	# fcl2_w = w_var([fcl1_dim,26])
+	# fcl2_b = b_var([26])
+	fcl2_dim = 128
+	fcl2_w = w_var([fcl1_dim,fcl2_dim])
+	fcl2_b = b_var([fcl2_dim])
+	fcl2 = tf.nn.relu(tf.matmul(dropout_fcl1,fcl2_w)+fcl2_b)
+	dropout_fcl2 = tf.nn.dropout(fcl2,keep_prob)
 
-	output = tf.matmul(dropout_fcl1,fcl2_w) + fcl2_b
+	# output = tf.matmul(dropout_fcl1,fcl2_w) + fcl2_b
+	# y_hat = tf.nn.softmax(output)
+	fcl3_w = w_var([fcl2_dim,26])
+	fcl3_b = b_var([26])
+	output = tf.matmul(dropout_fcl2,fcl3_w) + fcl3_b
 	y_hat = tf.nn.softmax(output)
 
 	#loss function and optimizer
@@ -104,10 +119,11 @@ def main():
 
 	correctness = tf.equal(tf.argmax(y_hat,1),tf.argmax(y,1))
 	accuracy = tf.reduce_mean(tf.cast(correctness,tf.float32))
+	predict = tf.argmax(y_hat,1)
 
 	# params for the execution
-	n_epochs = 20
-	batch_size = 200
+	n_epochs = 15
+	batch_size = 400
 	batch_number = int(ceil(len(train_data)/batch_size))
 	sess = tf.Session()
 	sess.run(tf.global_variables_initializer())
@@ -119,7 +135,20 @@ def main():
 			sess.run(train_opt,feed_dict={x:train_data[curr_idx],y:train_labels[curr_idx],keep_prob:0.5})
 
 	# accuracy on training set
-	print(sess.run(accuracy,feed_dict={x: train_data, y:train_labels, keep_prob: 1.0}))
+	for i in range(batch_number):
+		curr_idx = get_batch(idxs,i,batch_size)
+		print(sess.run(accuracy,feed_dict={x: train_data[curr_idx], y:train_labels[curr_idx], keep_prob: 0.5}))
+
+	results = []
+	idxs = list(range(len(test_data)))
+	# predict
+	for i in range(batch_number):
+		curr_idx = get_batch(idxs,i,batch_size)
+		results.append(sess.run(predict,feed_dict={x:test_data[curr_idx],keep_prob:0.5}))
+	with open(test_labels_path,'w') as file:
+		for value in results:
+			file.write(chr(value+ord('a'))+'\n')
+
 
 if __name__ == '__main__':
 	main()
